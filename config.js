d918a19a0d3cf5a9c1a0f71a1019c81000ed67a0
Asmit-Bhattacharjee <74710334+Asmit-Bhattacharjee@users.noreply.github.com>
2022-01-16 17:49:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDboLypT_sBiDyds9o-ap28Wr1bsNPzwZU",
+  authDomain: "storytellingapp-6d46f.firebaseapp.com",
+  databaseURL: "https://storytellingapp-6d46f-default-rtdb.firebaseio.com",
+  projectId: "storytellingapp-6d46f",
+  storageBucket: "storytellingapp-6d46f.appspot.com",
+  messagingSenderId: "81914307324",
+  appId: "1:81914307324:web:f4f97594856ee42acc6d79"
+};
